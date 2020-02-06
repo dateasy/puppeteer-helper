ddf521d8f6e375721dce01c34858a6b0b0143403
creditomotor <creditomotor>
2020-02-06 12:43:06 +0100

    First commit adding README AND the helper FILE to folder src/

---
+++ src/helper.js
@@ -0,0 +1,55 @@
+
+
+const helper = {
+    ElementHandle:null,
+    selector: null,
+    input: null,
+    options:null,
+
+    findSelectOption: async (ElementHandle,selector,input) => {
+        const options = await ElementHandle.evaluate(optionSelector => {
+            return Array.from(document.querySelectorAll(optionSelector + " > option"))
+                .filter(o => o.value)
+                .map(o => {
+                    return {
+                        text: o.text,
+                        value: o.value
+                    };
+                });
+        }, selector);
+
+        helper.ElementHandle    = ElementHandle;
+        helper.selector         = selector;
+        helper.input            = input;
+        helper.options          = options;
+        await helper.findElementOption();
+        //return options;
+    },
+    findElementOption: async () => {
+        for (let index = 0; index < helper.options.length; index++) {
+            const element = options[index];
+            if (element.name.toString().toLowerCase().trim() === helper.input.toString().toLowerCase().trim() )
+            {
+                await helper.ElementHandle.select(helper.selector,element.value);
+                return;
+            }
+        }
+        throw "Error: Couldnt find coincidence for selector:." + helper.selector + " and input :" + helper.input;
+    },
+    optionsToArray: async (ElementHandle,selector) => {
+        return await ElementHandle.evaluate(optionSelector => {
+            return Array.from(document.querySelectorAll(optionSelector + " > option"))
+                .filter(o => o.value)
+                .map(o => {
+                    return {
+                        text: o.text,
+                        value: o.value
+                    };
+                });
+        }, selector);
+    }
+
+}
+
+
+module.exports = helper;
